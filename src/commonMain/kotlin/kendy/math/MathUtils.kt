e32d4baf185a3d8424beb671ed9230324f7378d3
Jan Holesovsky <holesovsky@gmail.com>
2021-01-08 11:40:35 +0100

    Fixed MathUtils.

--- src/commonMain/kotlin/kendy/math/MathUtils.kt
+++ src/commonMain/kotlin/kendy/math/MathUtils.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package libcore.math
+package kendy.math
 
 object MathUtils {
     /**
