be07edd44c01d63bbb4e15e47575f0a2b0d6f2c3
Jan Holesovsky <kendy@collabora.com>
2021-04-02 23:13:40 +0200

    Add empty project structure.

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+    id("com.android.library")
+    id("kotlin-android-extensions")
+}
+
+group = "kendy.math"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    jcenter()
+    mavenCentral()
+}
+
+kotlin {
+    android()
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.2.1")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13")
+            }
+        }
+        val iosMain by getting
+        val iosTest by getting
+    }
+}
+
+android {
+    compileSdkVersion(29)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(29)
+    }
+}
